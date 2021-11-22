5f54152bd885ea678d003384faedd0199894e6f4
CalElAn <ubuntu@ip-172-31-0-31.us-east-2.compute.internal>
2021-11-22 07:13:18 +0000

    Trying to fix npx mix prod problem on server

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 class DeployController extends Controller
@@ -12,12 +13,24 @@ class DeployController extends Controller
      $githubPayload = $request->getContent();
      $githubHash = $request->header('X-Hub-Signature-256');
      $localToken = config('app.deploy_secret');
-     $localHash = 'sha1=' . hash_hmac('sha256', $githubPayload, $localToken, false);
+     $localHash = 'sha256=' . hash_hmac('sha256', $githubPayload, $localToken, false);
      if (hash_equals($githubHash, $localHash)) {
-          $root_path = base_path();
-          $process = new Process(['cd', $root_path, '; ./deploy.sh']);
-	  $process = new Process(['./deploy.sh']);
-          $process->run();
-     }
+          //$root_path = base_path();
+	  //$process = new Process(['cd', $root_path]);
+	  //$process->run();
+	  //$process = new Process(['./deploy.sh']);
+          //$process->run();
+	  /*$process = new Process(['npm', 'v']);
+	  $process->run();
+
+if (!$process->isSuccessful()) {
+    throw new ProcessFailedException($process);
+}
+
+echo $process->getOutput();*/
+	$output = shell_exec('cd '.base_path().'; ./deploy.sh');
+	echo $output;
+     } 
+
   }
 }
